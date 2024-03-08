d670e4928ea767af79f1811a1bbf7e0f40e221a3
shatkovskiii <shotdave169@gmail.com>
2024-03-08 14:49:05 +0300

    added views

--- Inserts/customer.sql
+++ Inserts/customer.sql
@@ -26,48 +26,12 @@ VALUES
   ('Ethan', 'Harris', 'ethan.harris@email.com'),
   ('Isabella', 'Moore', 'isabella.moore@email.com');
 
--- Запрос 21-30
-INSERT INTO Customer (firstname, lastname, contact_information)
-VALUES 
-  ('Noah', 'Clark', 'noah.clark@email.com'),
-  ('Olivia', 'White', 'olivia.white@email.com'),
-  ('Liam', 'Smith', 'liam.smith@email.com'),
-  ('Emma', 'Davis', 'emma.davis@email.com'),
-  ('Oliver', 'Johnson', 'oliver.johnson@email.com'),
-  ('Sophia', 'Williams', 'sophia.williams@email.com'),
-  ('William', 'Taylor', 'william.taylor@email.com'),
-  ('Ava', 'Brown', 'ava.brown@email.com'),
-  ('James', 'Miller', 'james.miller@email.com'),
-  ('Grace', 'Jones', 'grace.jones@email.com');
 
 
--- Запрос 11-20
-INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
-  ('Liam', 'Johnson', 'liam.johnson@email.com'),
-  ('Emma', 'Taylor', 'emma.taylor@email.com'),
-  ('James', 'Brown', 'james.brown@email.com'),
-  ('Sophie', 'Jones', 'sophie.jones@email.com'),
-  ('Alexander', 'Wilson', 'alexander.wilson@email.com'),
-  ('Ava', 'Miller', 'ava.miller@email.com'),
-  ('Matthew', 'Davis', 'matthew.davis@email.com'),
-  ('Grace', 'Anderson', 'grace.anderson@email.com'),
-  ('Ethan', 'Harris', 'ethan.harris@email.com'),
-  ('Isabella', 'Moore', 'isabella.moore@email.com');
+
+
 
 -- Запрос 21-30
-INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
-  ('Noah', 'Clark', 'noah.clark@email.com'),
-  ('Olivia', 'White', 'olivia.white@email.com'),
-  ('Liam', 'Smith', 'liam.smith@email.com'),
-  ('Emma', 'Davis', 'emma.davis@email.com'),
-  ('Oliver', 'Johnson', 'oliver.johnson@email.com'),
-  ('Sophia', 'Williams', 'sophia.williams@email.com'),
-  ('William', 'Taylor', 'william.taylor@email.com'),
-  ('Ava', 'Brown', 'ava.brown@email.com'),
-  ('James', 'Miller', 'james.miller@email.com'),
-  ('Grace', 'Jones', 'grace.jones@email.com');
-
--- Запрос 31-40
 INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
   ('Logan', 'Harris', 'logan.harris@email.com'),
   ('Chloe', 'Anderson', 'chloe.anderson@email.com'),
@@ -80,7 +44,7 @@ INSERT INTO Customer (firstname, lastname, contact_information) VALUES
   ('Elijah', 'Clark', 'elijah.clark@email.com'),
   ('Abigail', 'White', 'abigail.white@email.com');
 
--- Запрос 41-50
+-- Запрос 31-40
 INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
   ('Carter', 'Wilson', 'carter.wilson@email.com'),
   ('Penelope', 'Doe', 'penelope.doe@email.com'),
@@ -93,7 +57,7 @@ INSERT INTO Customer (firstname, lastname, contact_information) VALUES
   ('Gabriel', 'Anderson', 'gabriel.anderson@email.com'),
   ('Zoe', 'Taylor', 'zoe.taylor@email.com');
 
--- Запрос 51-60
+-- Запрос 41-50
 INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
   ('Luke', 'Miller', 'luke.miller@email.com'),
   ('Madison', 'Clark', 'madison.clark@email.com'),
@@ -105,56 +69,3 @@ INSERT INTO Customer (firstname, lastname, contact_information) VALUES
   ('Addison', 'Anderson', 'addison.anderson@email.com'),
   ('Joseph', 'Williams', 'joseph.williams@email.com'),
   ('Avery', 'Doe', 'avery.doe@email.com');
-
--- Запрос 61-70
-INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
-  ('Ethan', 'Taylor', 'ethan.taylor@email.com'),
-  ('Scarlett', 'Harris', 'scarlett.harris@email.com'),
-  ('James', 'Johnson', 'james.johnson@email.com'),
-  ('Zoe', 'Brown', 'zoe.brown@email.com'),
-  ('Benjamin', 'Clark', 'benjamin.clark@email.com'),
-  ('Abigail', 'Moore', 'abigail.moore@email.com'),
-  ('Henry', 'Smith', 'henry.smith@email.com'),
-  ('Luna', 'Anderson', 'luna.anderson@email.com'),
-  ('Lucas', 'Miller', 'lucas.miller@email.com'),
-  ('Mia', 'Jones', 'mia.jones@email.com');
-
--- Запрос 71-80
-INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
-  ('Emma', 'Doe', 'emma.doe@email.com'),
-  ('Liam', 'Johnson', 'liam.johnson@email.com'),
-  ('Ava', 'Clark', 'ava.clark@email.com'),
-  ('Oliver', 'Harris', 'oliver.harris@email.com'),
-  ('Sophia', 'Williams', 'sophia.williams@email.com'),
-  ('Noah', 'Brown', 'noah.brown@email.com'),
-  ('Isabella', 'Taylor', 'isabella.taylor@email.com'),
-  ('Ethan', 'Miller', 'ethan.miller@email.com'),
-  ('Grace', 'Moore', 'grace.moore@email.com'),
-  ('Logan', 'Smith', 'logan.smith@email.com');
-
--- Запрос 81-90
-INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
-  ('Aiden', 'Anderson', 'aiden.anderson@email.com'),
-  ('Chloe', 'Doe', 'chloe.doe@email.com'),
-  ('Gabriel', 'Williams', 'gabriel.williams@email.com'),
-  ('Lily', 'Harris', 'lily.harris@email.com'),
-  ('Grayson', 'Brown', 'grayson.brown@email.com'),
-  ('Zoe', 'Taylor', 'zoe.taylor@email.com'),
-  ('Carter', 'Smith', 'carter.smith@email.com'),
-  ('Penelope', 'Clark', 'penelope.clark@email.com'),
-  ('Jackson', 'Miller', 'jackson.miller@email.com'),
-  ('Evelyn', 'Moore', 'evelyn.moore@email.com');
-
--- Запрос 91-100
-INSERT INTO Customer (firstname, lastname, contact_information) VALUES 
-  ('Elijah', 'Johnson', 'elijah.johnson@email.com'),
-  ('Scarlett', 'Williams', 'scarlett.williams@email.com'),
-  ('Lucas', 'Harris', 'lucas.harris@email.com'),
-  ('Aria', 'Brown', 'aria.brown@email.com'),
-  ('Mia', 'Doe', 'mia.doe@email.com'),
-  ('Logan', 'Clark', 'logan.clark@email.com'),
-  ('Emma', 'Smith', 'emma.smith@email.com'),
-  ('Noah', 'Anderson', 'noah.anderson@email.com'),
-  ('Sophia', 'Taylor', 'sophia.taylor@email.com'),
-  ('William', 'Moore', 'william.moore@email.com');
-
--- Inserts/estate_agent.sql
+++ Inserts/estate_agent.sql
@@ -37,33 +37,10 @@ INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES
   ('Ava', 'Turner', 'ava.turner@email.com'),
   ('William', 'Harris', 'william.harris@email.com');
 
+
+
+
 -- Запрос 31-40
-INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES 
-  ('Madison', 'Anderson', 'madison.anderson@email.com'),
-  ('Gabriel', 'Doe', 'gabriel.doe@email.com'),
-  ('Luna', 'Johnson', 'luna.johnson@email.com'),
-  ('Evelyn', 'Smith', 'evelyn.smith@email.com'),
-  ('Grayson', 'Turner', 'grayson.turner@email.com'),
-  ('Mia', 'Brown', 'mia.brown@email.com'),
-  ('Liam', 'Miller', 'liam.miller@email.com'),
-  ('Isabella', 'Clark', 'isabella.clark@email.com'),
-  ('Lucas', 'Williams', 'lucas.williams@email.com'),
-  ('Sophia', 'Harris', 'sophia.harris@email.com');
-
--- Запрос 41-50
-INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES 
-  ('Ethan', 'Taylor', 'ethan.taylor@email.com'),
-  ('Scarlett', 'Anderson', 'scarlett.anderson@email.com'),
-  ('Jackson', 'Turner', 'jackson.turner@email.com'),
-  ('Chloe', 'Jones', 'chloe.jones@email.com'),
-  ('Benjamin', 'Harris', 'benjamin.harris@email.com'),
-  ('Zoe', 'Smith', 'zoe.smith@email.com'),
-  ('Carter', 'Brown', 'carter.brown@email.com'),
-  ('Penelope', 'Clark', 'penelope.clark@email.com'),
-  ('Oliver', 'Miller', 'oliver.miller@email.com'),
-  ('Aria', 'Johnson', 'aria.johnson@email.com');
-
--- Запрос 51-60
 INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES 
   ('Luke', 'Doe', 'luke.doe@email.com'),
   ('Madison', 'Harris', 'madison.harris@email.com'),
@@ -76,7 +53,7 @@ INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES
   ('Joseph', 'Williams', 'joseph.williams@email.com'),
   ('Avery', 'Doe', 'avery.doe@email.com');
 
--- Запрос 61-70
+-- Запрос 41-50
 INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES 
   ('Ethan', 'Taylor', 'ethan.taylor@email.com'),
   ('Scarlett', 'Harris', 'scarlett.harris@email.com'),
@@ -89,20 +66,8 @@ INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES
   ('Lucas', 'Miller', 'lucas.miller@email.com'),
   ('Mia', 'Jones', 'mia.jones@email.com');
 
--- Запрос 71-80
-INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES 
-  ('Emma', 'Doe', 'emma.doe@email.com'),
-  ('Liam', 'Johnson', 'liam.johnson@email.com'),
-  ('Ava', 'Clark', 'ava.clark@email.com'),
-  ('Oliver', 'Harris', 'oliver.harris@email.com'),
-  ('Sophia', 'Williams', 'sophia.williams@email.com'),
-  ('Noah', 'Brown', 'noah.brown@email.com'),
-  ('Isabella', 'Taylor', 'isabella.taylor@email.com'),
-  ('Ethan', 'Miller', 'ethan.miller@email.com'),
-  ('Grace', 'Moore', 'grace.moore@email.com'),
-  ('Logan', 'Smith', 'logan.smith@email.com');
 
--- Запрос 81-90
+-- Запрос 51-60
 INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES 
   ('Aiden', 'Anderson', 'aiden.anderson@email.com'),
   ('Chloe', 'Doe', 'chloe.doe@email.com'),
@@ -115,7 +80,7 @@ INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES
   ('Jackson', 'Miller', 'jackson.miller@email.com'),
   ('Evelyn', 'Moore', 'evelyn.moore@email.com');
 
--- Запрос 91
+-- Запрос 61
 INSERT INTO estate_agent (firstname, lastname, contact_information) VALUES 
   ('Elijah', 'Johnson', 'elijah.johnson@email.com');
 
